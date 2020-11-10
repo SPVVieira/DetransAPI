5673af25798152f924fb9fa457b8b3443dbb078c
Victor Vieira <victor.vieira@companyconferi.com.br>
2020-11-10 10:52:10 -0300

    separando APIS

--- src/controllers/detran/detranMS.js
+++ src/controllers/detran/detranMS.js
@@ -1,20 +1,80 @@
 const puppeteer = require('puppeteer');
 
-async function detranMS(placa, renavam) {
+const detranMS = async (placa, renavam) => {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         args: ['--disable-notifications', '--no-sandbox', '--disable-setuid-sandbox'],
         ignoreDefaultArgs: ['--mute-audio']
     });
     const page = await browser.newPage();
-    await page.goto('https://www.meudetran.ms.gov.br/veiculo.php');
-    await page.waitForTimeout(3000);
-    await page.evaluate((placa , renavam) => {
-        document.getElementById('id_sc_field_placa').value=placa;
-        document.getElementById('id_sc_field_renavam').value=renavam;
-        document.querySelectorAll('table > tbody > tr > td > a.scButton_default')[2].click();
-    }, placa, renavam);
+    await page.goto('https://www.meudetran.ms.gov.br/veiculo_debito_frm/index.php?where_ssn=Ativo%20=%201%20AND%20Placa=%27' + placa + '%27', {waitUntil:'networkidle2'});
+    await page.waitForSelector('td#hidden_field_data_placa_dumb');
+    await page.evaluate(() => {
+        placa = document.querySelectorAll('table > tbody > tr > td > span')[10].innerText;
+        renavam = document.querySelectorAll('table > tbody > tr > td > span')[12].innerText;
+        chassi = document.querySelectorAll('table > tbody > tr > td > span')[14].innerText;
+        cor = document.querySelectorAll('table > tbody > tr > td > span')[16].innerText;
+        categoria = document.querySelectorAll('table > tbody > tr > td > span')[18].innerText;
+        motor = document.querySelectorAll('table > tbody > tr > td > span')[20].innerText;
+        anoFabMod = document.querySelectorAll('table > tbody > tr > td > span')[22].innerText;
+        dataExpedicao = document.querySelectorAll('table > tbody > tr > td > span')[24].innerText;
+        licenciadoAte = document.querySelectorAll('table > tbody > tr > td > span')[26].innerText;
+        marca = document.querySelectorAll('table > tbody > tr > td > span')[28].innerText;
+        municipio = document.querySelectorAll('table > tbody > tr > td > span')[30].innerText;
+        licenciamentoDigital = document.querySelectorAll('table > tbody > tr > td > span')[32].innerText;
+        if(document.querySelectorAll('table > tbody > tr > td > span')[34].innerText == "NAO HA DEBITOS PARA ESTE VEICULO") {
+            observacoes = document.querySelectorAll('table > tbody > tr > td > span')[34].innerText;
+        }else {
+            boxObservacoes = document.querySelectorAll('table > tbody > tr > td')[76];
+            observacoes = [];
+            boxObservacoes.querySelectorAll('table > tbody > tr > td > span > li').forEach(element => {
+                observacoes.push(element.innerText);
+            });
+        }
 
+        objDadosVeiculo = {
+            'placa': placa,
+            'renavam': renavam,
+            'chassi': chassi,
+            'cor': cor,
+            'categoria': categoria,
+            'motor': motor,
+            'anoFabMod': anoFabMod,
+            'dataExpedicao': dataExpedicao,
+            'licenciadoAte': licenciadoAte,
+            'marca': marca,
+            'municipio': municipio,
+            'licenciamentoDigital': licenciamentoDigital,
+            'observacoes': observacoes
+            
+        };
+
+        debitosGerais = document.querySelectorAll('table > tbody > tr > td >table > tbody > tr > td')[43].innerText;
+        retornoDebitos = [];
+        retornoLinhas = debitosGerais.split("Valor (R$)")[1].trim().split("\n").forEach(debitos => {
+            if(debitos.trim() != ''){
+                retornoDebitos.push(debitos.trim());
+            }
+        });
+        totalGeral = debitosGerais.split('TOTAL GERAL')[1].trim();
+
+        objDebitos = {
+            'totalGeral': totalGeral,
+            'debitos': retornoDebitos
+        };
+
+        return {
+            'dadosVeiculo': objDadosVeiculo,
+            'debitosVeiculo': objDebitos
+        }
+    }).then((retornoVeiculo) => {
+        console.log(retornoVeiculo);
+    }).catch((err) => {
+        console.log('ERRO ' + err);
+    })
+    await page.waitForTimeout(500);
+    await page.close();
+    await browser.close();
 }
 
-detranMS('hma0001', '00149177178');
+module.exports = detranMS;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const mail = require('./controllers/nfs/mail');
-const nota = require('./controllers/nfs/nota');
 
 const dividaBA = require('./controllers/divida/dividaBA');
 const detranDF = require('./controllers/detran/detranDF');
+const detranMS = require('./controllers/detran/detranMS');
 
 const app = express();
 
 app.use(cors({origin: 'https://www.companyconferi.com.br/'}));
+
 //HOME
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/view/index.html');
 });
+
 //EXPLICAÇÃO NOTA 
 app.get('/notaFiscal', (req,res) => {
     res.sendFile(__dirname + '/view/notaFiscal.html');
 });
+
 //EXPLICAÇÃO DETRAN
 app.get('/detran', (req,res) => {
     res.sendFile(__dirname + '/view/detran.html');
 });
+
 //EXPLICAÇÃO DIVIDA
 app.get('/dividaAtiva', (req,res) => {
     res.sendFile(__dirname + '/view/dividaAtiva.html');
 });
+
 //ROTA DIVIDA BA
 app.get('/dividaAtiva/BA/:placa/:renavam', (req,res) => {
     dividaBA(req.params.placa, req.params.renavam).then((ret) => {
@@ -33,6 +37,7 @@ app.get('/dividaAtiva/BA/:placa/:renavam', (req,res) => {
         res.json({'retorno': 0, 'Mensagem': 'Sem retorno'});
     })
 });
+
 //ROTA DETRAN DF
 app.get('/detran/DF/:placa/:renavam', (req,res) => {
     detranDF(req.params.placa, req.params.renavam).then((ret) => {
@@ -49,47 +54,6 @@ app.get('/detran/MS/:placa/:renavam', (req,res) => {
     }).catch(() => {
         res.json({'retorno': 0, 'Mensagem': 'Sem retorno'});
     })
-});
-
-//ROTA NOTA FISCAL
-app.get('/notaFiscal/emissao', (req,res) => {
-        /*retornoDados = {
-            'codigoCliente': '123',
-            'empresaCadastro': 'CREDCOMPANY',
-            'tipoDocumento': 'CPF',
-            'documento': '46219956850',
-            'razaoSocial': 'Victor Hugo de Paula Vieira',
-            'inscricao': '',
-            'cep': '08900000',
-            'cidade': 'GUARAREMA',
-            'estado': 'SÃO PAULO',
-            'bairro': 'Centro',
-            'logradouro': 'Rua peixoto',
-            'numeroEnd': '371',
-            'complemento': 'Casa 02',
-            'telefone': '11942415211',
-            'email': 'victor10jb@hotmail.com',
-            'valor': '100,00',
-            'valorTributado': '18,45',
-            'aliquota': '3,34'
-        };*/
-        /*nota(retornoDados).then((result)=>{
-            mail(result, retornoDados).then((respostaEmail) => {
-                if(respostaEmail.accepted == retornoDados.email){
-                    res.send({"retorno": 1, "Resposta": "Email enviado"});
-                }else if(respostaEmail.rejected == retornoDados.email){
-                    res.send({"retorno": 0, "Resposta": "Falha no envio do email"});
-                }else{
-                    res.send(respostaEmail);
-                }
-            }).catch((err) => {
-                console.log("ERRO EMAIL: " + err);
-                res.send({"retorno": 0, "Resposta": "Falha no envio do email"});
-            });
-        }).catch((err) => {
-            console.log("ERRO EMISSÃO NOTA: " + err);
-            res.send({"retorno": 0, "Resposta": "Falha na emissao da nota"});
-        });*/
 });
 
 var port = process.env.PORT || 3000;
